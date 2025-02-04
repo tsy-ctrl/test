5d5787bb145e6bcc4f4eb438ff6e302da101d188
tsy <75145981+ppleaser@users.noreply.github.com>
2025-02-04 15:19:14 +0200

    Update crop.py

--- crop.py
+++ crop.py
@@ -1338,16 +1338,22 @@ async def process_messages_for_author(
                 output_file.write(hints_html)
             
                 output_file.write(f'''
-                <div class="auto-delete-container">
-                    <div class="toggle-switch {'active' if AUTO_DELETE_ENABLED else ''}" 
-                        onclick="toggleAutoDelete()"
-                        style="background-color: {'#488b5b' if AUTO_DELETE_ENABLED else '#d9534f'}">
-                        <div class="slider"></div>
+                       <div class="auto-delete-container">
+                        <div class="toggle-switch {'active' if AUTO_DELETE_ENABLED else ''}" 
+                            onclick="toggleAutoDelete()" >
+                            <div class="slider">
+                                <svg class="icon-shield" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path d="M20 6C20 6 19.1843 6 19.0001 6C16.2681 6 13.8871 4.93485 11.9999 3C10.1128 4.93478 7.73199 6 5.00009 6C4.81589 6 4.00009 6 4.00009 6C4.00009 6 4 8 4 9.16611C4 14.8596 7.3994 19.6436 12 21C16.6006 19.6436 20 14.8596 20 9.16611C20 8 20 6 20 6Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>          
+                                <svg class="icon-trash" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                <path d="M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6M14 10V17M10 10V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="file-stats">
+                            <span id="file-count">0</span>files | <span id="file-size">0 B</span>
+                        </div>
                     </div>
-                    <div class="file-stats">
-                        <span id="file-count">0</span>files | <span id="file-size">0 B</span>
-                    </div>
-                </div>
                 ''')
                  
             except Exception as e:
